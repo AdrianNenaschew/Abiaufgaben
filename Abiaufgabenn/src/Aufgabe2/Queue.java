dea07c2f507ee7719fdb187deb529f164f5dbf4f
AdrianNenaschew <AdrianNenaschew@0-209-13>
2022-10-07 09:07:13 +0200

    Abiaufgabe B3

--- Abiaufgabenn/src/Aufgabe2/Queue.java
+++ Abiaufgabenn/src/Aufgabe2/Queue.java
@@ -1,5 +1,18 @@
 package Aufgabe2;
 
 public class Queue {
+private LinkedList first = null;
 
+	public void enqueue(Object d) {
+		LinkedList n = new LinkedList(d, null);
+		if (first == null) {
+			first = n;
+		} else {
+			LinkedList l = first;
+			while (l.getNext() != null) {
+				l = l.getNext();
+			}
+			l.setNext(n);
+		}
+	}
 }
